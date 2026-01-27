9d28937179c567ce3e4502d9d3d71da84b4626be
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-01-28 06:01:07 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 14804 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            int start = 0;
+            int end = N - 1;
+
+            while (start < end) {
+                if (start == i) {
+                    start++;
+                    continue;
+                }
+
+                if (end == i) {
+                    end--;
+                    continue;
+                }
+
+                if (arr[start] + arr[end] == arr[i]) {
+                    count++;
+                    break;
+                }
+                else if (arr[start] + arr[end] < arr[i]) {
+                    start++;
+                }
+                else {
+                    end--;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
