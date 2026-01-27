85647d36eb695b2e67914458dd250474860105a8
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-01-27 15:32:58 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16876 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int count = 0;
+        int start = 0;
+        int end = N - 1;
+
+        while (start < end) {
+            int sum = arr[start] + arr[end];
+
+            if (sum == M) {
+                count++;
+                start++;
+                end--;
+            }
+            else if (sum < M) {
+                start++;
+            }
+            else {
+                if (start > 0) start--;
+                end--;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
