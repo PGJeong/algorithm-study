5a43ce44ab7d3be793f2cdbe16d980962e2cd479
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-01-28 18:49:07 +0900

    [Silver II] Title: DNA 비밀번호, Time: 224 ms, Memory: 20200 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -4,15 +4,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
+    static int S, P, passwords;
+    static int[] count, minCount;
+
+    private static void addChar(char c) {
+        switch (c) {
+            case 'A': count[0]++; break;
+            case 'C': count[1]++; break;
+            case 'G': count[2]++; break;
+            case 'T': count[3]++; break;
+        }
+    }
+
+    private static void removeChar(char c) {
+        switch (c) {
+            case 'A': count[0]--; break;
+            case 'C': count[1]--; break;
+            case 'G': count[2]--; break;
+            case 'T': count[3]--; break;
+        }
+    }
+
+    private static void checkValid() {
+        if (count[0] >= minCount[0] &&
+            count[1] >= minCount[1] &&
+            count[2] >= minCount[2] &&
+            count[3] >= minCount[3]) {
+            passwords++;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int S = Integer.parseInt(st.nextToken()); // 문자열 길이
-        int P = Integer.parseInt(st.nextToken()); // 비밀번호 길이
-        String str = br.readLine();     // 문자열
-        int[] count = new int[4];       // A,C,G,T 개수 카운트
-        int[] minCount = new int[4];    // A,C,G,T 최소 개수
+        S = Integer.parseInt(st.nextToken()); // 문자열 길이
+        P = Integer.parseInt(st.nextToken()); // 비밀번호 길이
+        String str = br.readLine(); // 문자열
+        count = new int[4];         // A,C,G,T 개수 카운트
+        minCount = new int[4];      // A,C,G,T 최소 개수
 
         st = new StringTokenizer(br.readLine());
 
@@ -20,50 +50,21 @@ class Main {
             minCount[i] = Integer.parseInt(st.nextToken());
         }
 
-        int passwords = 0;
+        passwords = 0;
 
         for (int i = 0; i < P; i++) {
-            char ch = str.charAt(i);
-
-            switch (ch) {
-                case 'A': count[0]++; break;
-                case 'C': count[1]++; break;
-                case 'G': count[2]++; break;
-                case 'T': count[3]++; break;
-            }
+            addChar(str.charAt(i));
         }
 
-        if (count[0] >= minCount[0] &&
-            count[1] >= minCount[1] &&
-            count[2] >= minCount[2] &&
-            count[3] >= minCount[3]) {
-            passwords++;
-        }
+        checkValid();
 
         for (int i = P; i < S; i++) {
             char prevCh = str.charAt(i - P);
             char newCh = str.charAt(i);
 
-            switch (prevCh) {
-                case 'A': count[0]--; break;
-                case 'C': count[1]--; break;
-                case 'G': count[2]--; break;
-                case 'T': count[3]--; break;
-            }
-
-            switch (newCh) {
-                case 'A': count[0]++; break;
-                case 'C': count[1]++; break;
-                case 'G': count[2]++; break;
-                case 'T': count[3]++; break;
-            }
-
-            if (count[0] >= minCount[0] &&
-                count[1] >= minCount[1] &&
-                count[2] >= minCount[2] &&
-                count[3] >= minCount[3]) {
-                passwords++;
-            }
+            removeChar(prevCh);
+            addChar(newCh);
+            checkValid();
         }
 
         System.out.println(passwords);
