5a3e1a64861efe3f1a768fa13f0e495835a20814
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-02-03 18:50:45 +0900

    [Gold I] Title: 최솟값 찾기, Time: 1980 ms, Memory: 641100 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,45 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
+
+class Node {
+    int index, value;
+
+    public Node(int index, int value) {
+        this.index = index;
+        this.value = value;
+    }
+}
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder result = new StringBuilder();
 
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
-        int[] res = new int[N];
+        Deque<Node> deque = new ArrayDeque<>();
 
         st = new StringTokenizer(br.readLine());
 
-        // [0]: number, [1]: index
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
-            }
-        });
-
         for (int i = 0; i < N; i++) {
-            pq.add(new int[]{Integer.parseInt(st.nextToken()), i});
+            int num = Integer.parseInt(st.nextToken());
 
-            while (pq.peek()[1] < (i-L+1)) {
-                pq.poll();
+            if (!deque.isEmpty() && deque.getFirst().index < i-L+1) {
+                deque.removeFirst();
             }
 
-            res[i] = pq.peek()[0];
+            while (!deque.isEmpty() && deque.getLast().value > num) {
+                deque.removeLast();
+            }
+
+            deque.addLast(new Node(i, num));
+
+            result.append(deque.getFirst().value).append(" ");
         }
 
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < N; i++) {
-            sb.append(res[i]).append(" ");
-        }
-
-        System.out.println(sb);
+        System.out.println(result);
     }
 }
