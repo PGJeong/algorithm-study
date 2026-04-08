faf1d3810bd5eedf7551a6bf8a3697b4116122c3
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-04-08 23:05:39 +0900

    [Gold II] Title: 트리의 지름, Time: 700 ms, Memory: 102428 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,87 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-class Edge {
-	public int vertex, dist;
-	public Edge(int vertex, int dist) { this.vertex = vertex; this.dist = dist; }
-}
+class Main {
+    static ArrayList<int[]>[] graph;
+    static int V, vertex, weight;
+    static boolean[] visited;
 
-public class Main {
-	// Memo: 트리를 인접행렬로 구현 시 메모리 초과 -> 인접리스트로 구현
-	private static int v;
-	private static ArrayList<Edge>[] tree;
-	private static boolean[] visited;
-	
-	private static void init() {
-		Scanner sc = new Scanner(System.in);
-		
-		v = sc.nextInt() + 1; // 정점 번호는 1부터 시작
-		tree = new ArrayList[v];
-		
-		for(int i = 1; i < v; i++)
-			tree[i] = new ArrayList<Edge>();
-		
-		for(int i = 1; i < v; i++) {
-			int a = sc.nextInt();
-			while(true) {
-				int b = sc.nextInt();
-				
-				if(b == -1) break;
-				
-				int dist = sc.nextInt();
-				tree[a].add(new Edge(b, dist));
-			}
-		}
-		
-		sc.close();
-	}
-	
-	private static Edge BFS(int start) {
-		// start 정점을 기준으로 가장 거리가 먼 "정점"과 "거리"를 return
-		int max = 0;
-		int vertex = start;
-		
-		Queue<Edge> q = new LinkedList<>();
-		
-		// 시작정점 삽입
-		q.add(new Edge(start, 0));
-		visited[start] = true;
-		
-		// BFS 탐색
-		while(!q.isEmpty()) {
-			Edge next = q.poll();
-			
-			// start 정점기준 최대 거리 갱신
-			int tmp = Math.max(max, next.dist);
-			if(max != tmp) { // 최대값 갱신이 필요한 경우
-				max = tmp;
-				vertex = next.vertex;
-			}
-			
-			for(Edge e : tree[next.vertex]) {
-				if(visited[e.vertex] == false) {
-					// start 정점부터 e 정점까지의 거리 합산
-					q.add(new Edge(e.vertex, e.dist + next.dist));
-					visited[e.vertex] = true;
-				}
-			}
-		}
-		
-		return new Edge(vertex, max);
-	}
-	
-	public static void main(String[] args) {
-		init();
-		
-		// (1.) 임의의 노드에서 가장 긴 경로로 연결된 노드는 트리의 지름에 해당하는 두 노드 중 하나
-		visited = new boolean[v];
-		Edge a = BFS(1);	
-		// (2.) a를 기준으로 가장 거리가 먼 정점까지의 거리는 트리의 지름
-		visited = new boolean[v];
-		Edge b = BFS(a.vertex);
-		
-		System.out.println(b.dist);
-	}
+    static void dfs(int v, int w) {
+        visited[v] = true;
+
+        if (w > weight) {
+            weight = w;
+            vertex = v;
+        }
+
+        for (int[] edge : graph[v]) {
+            if (!visited[edge[0]]) {
+                dfs(edge[0], w + edge[1]);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[V + 1];
+
+        for (int i = 1; i <= V; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= V; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+
+            while (true) {
+                int to = Integer.parseInt(st.nextToken());
+
+                if (to == -1) {
+                    break;
+                }
+
+                int weight = Integer.parseInt(st.nextToken());
+
+                graph[from].add(new int[] {to, weight});
+            }
+        }
+
+        weight = 0;
+        visited = new boolean[V + 1];
+        dfs(1, 0);
+
+        weight = 0;
+        visited = new boolean[V + 1];
+        dfs(vertex, 0);
+
+        System.out.println(weight);
+    }
 }
