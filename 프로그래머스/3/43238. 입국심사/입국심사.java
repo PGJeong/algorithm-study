16b8e59776bd137b7e8753b84fe738c1d3c62d06
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-04-24 03:10:35 +0900

    [level 3] Title: 입국심사, Time: 95.47 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,30 +1,23 @@
 import java.util.*;
 
 class Solution {
-    // 시간 내 n명의 사람을 심사할 수 있는지 여부
-    boolean check(int n, long time, int[] times) {
-        long cnt = 0;
-        
-        for (int t : times) {
-            cnt += time / t;
-        }
-        
-        if (cnt < n) return false;
-        else return true;
-    }
-    
     public long solution(int n, int[] times) {
-        long answer = 0;
-        
         Arrays.sort(times);
         
+        long answer = 0;
         long min = 0;
         long max = (long) times[times.length - 1] * n;
         
         while (min <= max) {
             long mid = (min + max) / 2;
             
-            if (check(n, mid, times)) {
+            long cnt = 0; // mid 시간 내에 처리할 수 있는 사람 수
+            
+            for (int t : times) {
+                cnt += mid / t;
+            }
+            
+            if (cnt >= n) {
                 // mid 시간에 n명의 사람을 처리할 수 있으면
                 answer = mid;
                 max = mid - 1;
