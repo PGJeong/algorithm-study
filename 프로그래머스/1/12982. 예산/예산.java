ff1f264f2c819edbc3d73236c83092381379a8f9
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-04-25 01:01:39 +0900

    [level 1] Title: 예산, Time: 1.34 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -4,12 +4,32 @@ class Solution {
     public int solution(int[] d, int budget) {
         Arrays.sort(d);
         
-        int answer = 0;
+        int min = 0;
+        int max = d.length;
+        int answer = -1;
         
-        for (int req : d) {
-            if (budget - req >= 0) {
-                budget -= req;
-                answer++;
+        while (min <= max) {
+            int mid = min + (max - min) / 2;
+            
+            int bud = budget;
+            int cnt = 0;
+            
+            for (int req : d) {
+                if (bud - req >= 0) {
+                    bud -= req;
+                    cnt++;
+                }
+                
+                if (cnt >= mid) {
+                    break;
+                }
+            }
+            
+            if (cnt >= mid) {
+                min = mid + 1;
+                answer = mid;
+            } else {
+                max = mid - 1;
             }
         }
         
