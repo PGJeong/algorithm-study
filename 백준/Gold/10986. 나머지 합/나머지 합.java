765db00daaf61ad90539a1aae0c55e8e9da1265c
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-04-27 23:02:01 +0900

    [Gold III] Title: 나머지 합, Time: 456 ms, Memory: 120680 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -3,28 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        long[] sums = new long[N + 1];
-        long[] count = new long[M];
+        long[] prefixSum = new long[N + 1];
+        long[] modCount = new long[M];
 
         st = new StringTokenizer(br.readLine());
 
         for (int i = 1; i <= N; i++) {
-            sums[i] = sums[i - 1] + Integer.parseInt(st.nextToken());
-            count[(int) (sums[i] % M)]++;
+            prefixSum[i] = (prefixSum[i - 1] + Integer.parseInt(st.nextToken())) % M;
+            modCount[(int)prefixSum[i]]++;
         }
 
-        long answer = count[0];
+        long answer = modCount[0];
 
         for (int i = 0; i < M; i++) {
-            long num = count[i];
-            answer += num * (num - 1) / 2;
+            answer += modCount[i] * (modCount[i] - 1) / 2;
         }
 
         System.out.println(answer);
