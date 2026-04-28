7526e153ff052ef7d89dad30fec14efea697c11a
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-04-28 16:26:27 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 18.98 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,37 +1,29 @@
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        // 1. 부분합 배열 생성
-        int[] psum = new int[sequence.length + 1];
+        int s = 0;
+        int e = 0;
+        int sum = sequence[0];
         
-        for (int i = 1; i < psum.length; i++) {
-            psum[i] = psum[i - 1] + sequence[i - 1];
-        }
+        int[] seq = new int[2];
+        int len = Integer.MAX_VALUE;
         
-        // 2. 투포인터 탐색
-        int s = 1; // 시작 인덱스(1-based)
-        int e = 1; // 종료 인덱스(1-based)
-        
-        int[] seq = null; // 부분수열 index(0-based)
-        int len = Integer.MAX_VALUE; // 부분수열 길이
-        
-        while (s <= e && e < psum.length) {
-            int sum = psum[e] - psum[s - 1];
-            
+        while (s <= e && e < sequence.length) {
             if (sum == k) {
-                // 부분수열 합이 k이면
-                if ((e - s + 1) < len) {
-                    seq = new int[] {s - 1, e - 1}; // 0-based 변환
+                if (e - s + 1 < len) {
+                    seq[0] = s;
+                    seq[1] = e;
                     len = e - s + 1;
                 }
                 e++;
- 
+                if (e < sequence.length) sum += sequence[e];
+                
             } else if (sum < k) {
-                // 부분수열 합이 k보다 작으면
                 e++;
+                if (e < sequence.length) sum += sequence[e];
                 
             } else {
-                // 부분수열 합이 k보다 크면
                 s++;
+                sum -= sequence[s - 1];
             }
         }
         
