51f388032abbbb907a34e31ebb89b695b0cc64bd
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-06-25 23:22:46 +0900

    [level 1] Title: 폰켓몬, Time: 2.13 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -8,7 +8,6 @@ class Solution {
             set.add(n);
         }
         
-        if (set.size() > (nums.length / 2)) return nums.length / 2;
-        else return set.size();
+        return Math.min(set.size(), nums.length / 2);
     }
 }
