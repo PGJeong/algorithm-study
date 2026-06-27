e55507682cc031b4230efccd452a539b3747a7af
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-06-27 22:00:13 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -6,15 +6,13 @@ class Solution {
         
         for (String[] cloth : clothes) {
             String type = cloth[1];
-            
-            if (map.containsKey(type)) map.put(type, map.get(type) + 1);
-            else map.put(type, 1);
+            map.put(type, map.getOrDefault(type, 0) + 1);
         }
         
         int count = 1;
         
-        for (String type : map.keySet()) {
-            count *= map.get(type) + 1; // 해당 종류를 안입는 경우 추가
+        for (int n : map.values()) {
+            count *= n + 1; // 해당 종류를 안입는 경우 추가
         }
         
         return count - 1; // 모두 안입는 경우 제외
