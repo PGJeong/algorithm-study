d6069529bc865d4fa962d5cded6e6706373779a1
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-06-27 19:27:57 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,6 @@
 import java.util.*;
 
-class Solution {
-    int[] count;
-    int answer = 0;
-    
-    void dfs(int sum, int index) {
-        if (index == count.length) {
-            answer += sum; // 경우의 수 합산
-            return;
-        }
-        
-        dfs(sum * count[index], index + 1); // 해당 종류를 착용한 경우
-        dfs(sum, index + 1); // 해당 종류를 착용하지 않은 경우
-    }
-    
+class Solution {  
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap<>();
         
@@ -24,15 +11,12 @@ class Solution {
             else map.put(type, 1);
         }
         
-        count = new int[map.size()];
-        int index = 0;
+        int count = 1;
         
-        for (String key : map.keySet()) {
-            count[index] = map.get(key);
-            index++;
+        for (String type : map.keySet()) {
+            count *= map.get(type) + 1; // 해당 종류를 안입는 경우 추가
         }
         
-        dfs(1, 0);
-        return answer - 1; // 모두 선택 안하는 경우 제외
+        return count - 1; // 모두 안입는 경우 제외
     }
 }
