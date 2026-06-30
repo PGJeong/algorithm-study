91e448cf6a92b75fce560f4e136cc4b3657ea810
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-07-01 03:05:28 +0900

    [level 3] Title: 이중우선순위큐, Time: 30.50 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,78 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> min = new PriorityQueue<>((n1, n2) -> n1 - n2); // 최솟값 PQ
-        PriorityQueue<Integer> max = new PriorityQueue<>((n1, n2) -> n2 - n1); // 최댓값 PQ
-        HashMap<Integer, Integer> cnt = new HashMap<>(); // 각 숫자의 개수
+        TreeMap<Integer, Integer> map = new TreeMap<>(); // (숫자, 개수) 쌍
         
-        for (String s : operations) {
-            StringTokenizer st = new StringTokenizer(s);
-            String op = st.nextToken(); // 명령어
-            int num = Integer.parseInt(st.nextToken()); // 데이터
-            
-            if (op.equals("I")) {
+        for (String op : operations) {
+            if (op.startsWith("I ")) {
                 // 삽입
-                min.add(num);
-                max.add(num);
-                cnt.put(num, cnt.getOrDefault(num, 0) + 1);
+                int num = Integer.parseInt(op.substring(2));
+                map.put(num, map.getOrDefault(num, 0) + 1);
                 
-            } else {
-                // 삭제
-                if (num == 1) {
-                    // 최댓값 삭제
-                    while (!max.isEmpty()) {
-                        int tmp = max.poll();
-                        
-                        if (cnt.get(tmp) <= 0) {
-                            continue;
-                            
-                        } else {
-                            cnt.put(tmp, cnt.get(tmp) - 1);
-                            break;
-                        }
-                    }
-                    
-                } else {
-                    // 최솟값 삭제
-                    while (!min.isEmpty()) {
-                        int tmp = min.poll();
-                        
-                        if (cnt.get(tmp) <= 0) {
-                            continue;
-                            
-                        } else {
-                            cnt.put(tmp, cnt.get(tmp) - 1);
-                            break;
-                        }
-                    }
-                }
+            } else if (!map.isEmpty() && op.equals("D 1")) {
+                // 최댓값 삭제
+                int max = map.lastKey();
+                map.put(max, map.get(max) - 1);
+                
+                if (map.get(max) <= 0) map.remove(max);
+                
+            } else if (!map.isEmpty() && op.equals("D -1")) {
+                // 최솟값 삭제
+                int min = map.firstKey();
+                map.put(min, map.get(min) - 1);
+                
+                if (map.get(min) <= 0) map.remove(min);
             }
         }
         
-        int[] answer = {0, 0};
-        
-        while (!max.isEmpty()) {
-            answer[0] = max.poll();
-            
-            if (cnt.get(answer[0]) <= 0) {
-                answer[0] = 0;
-                continue;
-            } else {
-                break;
-            }
-        }
-        
-        while (!min.isEmpty()) {
-            answer[1] = min.poll();
-            
-            if (cnt.get(answer[1]) <= 0) {
-                answer[1] = 0;
-                continue;
-            } else {
-                break;
-            }
-        }
-        
-        return answer;
+        if (map.isEmpty()) return new int[] {0, 0};
+        else return new int[] {map.lastKey(), map.firstKey()};
     }
 }
