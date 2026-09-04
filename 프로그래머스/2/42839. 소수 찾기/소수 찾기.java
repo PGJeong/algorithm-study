cdc4b78565178ed4ea9a51e4443287641795fac8
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-09-04 13:00:00 +0900

    [level 2] Title: 소수 찾기, Time: 56.13 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    boolean[] isSelected;
+    HashSet<Integer> checked;
+    int count = 0;
+    
+    void checkPrime(int n) {
+        if (checked.contains(n)) return;
+        checked.add(n);
+        
+        if (n < 2) return;
+        
+        for (int i = 2; i < (n / 2) + 1; i++) {
+            if (n % i == 0) return;
+        }
+        
+        count++;
+    }
+    
+    void makeNumber(String n, String numbers) {
+        // 현재 번호 확인
+        checkPrime(Integer.parseInt(n));
+        
+        // 자릿수 추가
+        for (int i = 0; i < numbers.length(); i++) {
+            if (!isSelected[i]) {
+                isSelected[i] = true;
+                makeNumber(n + numbers.charAt(i), numbers);
+                makeNumber(n, numbers);
+                isSelected[i] = false;
+            }
+        }
+    }
+    
+    public int solution(String numbers) {
+        isSelected = new boolean[numbers.length()];
+        checked = new HashSet<>();
+        
+        for (int i = 0; i < numbers.length(); i++) {
+            isSelected[i] = true;
+            makeNumber("" + numbers.charAt(i), numbers);
+            isSelected[i] = false;
+        }
+        
+        return count;
+    }
+}
