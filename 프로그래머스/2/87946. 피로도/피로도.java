8d768c55874639c1bf976e444c06830811d1e3c6
Pilgyo Jeong <110081523+PGJeong@users.noreply.github.com>
2026-09-17 23:07:54 +0900

    [level 2] Title: 피로도, Time: 1.62 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -18,14 +18,7 @@ class Solution {
     
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-        
-        for (int i = 0; i < dungeons.length; i++) {
-            if (k >= dungeons[i][0]) {
-                visited[i] = true;
-                explore(k - dungeons[i][1], dungeons, 1);
-                visited[i] = false;
-            }
-        }
+        explore(k, dungeons, 0);
         
         return max;
     }
